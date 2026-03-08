d0464a1559e740cb7712f7f17b8f712ff1383e06
moataz3mad <mo3taz3mad@gmail.com>
2026-03-08 03:25:40 +0200

    contact-us component, AOS and bootstrap

--- ASMECairoUniversityFrontEnd/src/app/app.ts
+++ ASMECairoUniversityFrontEnd/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContactUs } from '../components/contact-us/contact-us';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ ContactUs, RouterOutlet ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- ASMECairoUniversityFrontEnd/src/components/contact-us/contact-us.ts
+++ ASMECairoUniversityFrontEnd/src/components/contact-us/contact-us.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-contact-us',
-  imports: [],
+  imports: [CommonModule, RouterModule,],
   templateUrl: './contact-us.html',
   styleUrl: './contact-us.css',
 })
 export class ContactUs {
+ngOnInit() {
+  AOS.init();
+}
 
 }
