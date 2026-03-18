9e1f1278dbb5de30264ea253928612c6534ea635
yousef <yyoousef1254@gmail.com>
2026-03-19 01:42:02 +0200

    ..

--- ASMECairoUniversityFrontEnd/src/app/app.ts
+++ ASMECairoUniversityFrontEnd/src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ContactUs } from './components/contact-us/contact-us';
+import { ContactUs } from '../components/contact-us/contact-us';
 import { Committee } from '../components/committee/committee';
 import { OurProject } from '../components/our-projects/our-projects';
 import { AboutAsme } from '../components/about-asme/about-asme';
