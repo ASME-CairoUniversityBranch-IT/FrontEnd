dcc2a5705a32c76e6268d83615abc62df7dcff0c
yousef <yyoousef1254@gmail.com>
2026-05-07 12:55:15 +0300

    solve conflict

--- ASMECairoUniversityFrontEnd/src/app/app.ts
+++ ASMECairoUniversityFrontEnd/src/app/app.ts
@@ -9,19 +9,13 @@ import { OurValues } from '../components/our-values/our-values';
 import { Navbar } from '../components/navbar/navbar';
 import { ActivitiesAchievements } from '../components/activities-achievements/activities-achievements';
 import { MainBanner } from '../components/main-banner/main-banner';
-<<<<<<< HEAD
 import { EventsPageComponent } from '../components/projectss-page/events-page';
-@Component({
-  selector: 'app-root',
-  imports: [ContactUs, RouterOutlet, OurProject, Committee, AboutAsme, MainFooter, OurValues, Navbar, ActivitiesAchievements, MainBanner, EventsPageComponent],
-=======
 import { CreateEventComponent } from "../components/create-event/create-event";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ContactUs, RouterOutlet, OurProject, Committee, AboutAsme, MainFooter, OurValues, Navbar, ActivitiesAchievements, MainBanner, CreateEventComponent],
->>>>>>> 5c8e2d1aaae277c4221440a74865a81052cf650a
+  imports: [ContactUs, RouterOutlet, OurProject, Committee, AboutAsme, MainFooter, OurValues, Navbar, ActivitiesAchievements, MainBanner, CreateEventComponent, EventsPageComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
