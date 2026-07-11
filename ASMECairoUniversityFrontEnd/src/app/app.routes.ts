a9f1956cda7f8401f5e0f59a2581e4533374a325
ZyadHamed <zyadhamedashour@gmail.com>
2026-07-12 01:25:50 +0300

    Made front end fixes

--- ASMECairoUniversityFrontEnd/src/app/app.routes.ts
+++ ASMECairoUniversityFrontEnd/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { OurValues } from '../components/our-values/our-values';
+import { EventsPageComponent } from '../components/projectss-page/events-page';
+import { HomePage } from '../components/home-page/home-page';
+import { CreateEventComponent } from '../components/create-event/create-event';
 
 export const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'our-values',component: OurValues},
+  {path: '', component: HomePage, pathMatch: 'full'},
+  {path: 'events',component: EventsPageComponent},
+  {path: 'events/create',component: CreateEventComponent},
 ];
